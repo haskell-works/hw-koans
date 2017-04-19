ce3284f30bea166c835382b40bcadd375a060295
John Ky <newhoggy@gmail.com>
2017-04-19 21:22:41 +1000

    New Eq suite

---
+++ koan/eq/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import           Control.Monad
+import           Data.Monoid
+import qualified Koan.Eq
+import           Lib
+
+tests =
+  [ Koan.Eq.tests
+  ]
+
+main :: IO ()
+main = do
+  results <- forM tests id
+  let successes = countElem True results
+  let failures  = countElem False results
+  let suites = successes + failures
+  if failures == 0
+    then putStrLn $ "All test suites succeeded"
+    else putStrLn $ show failures <> " out of " <> show suites <> " test suites failed"

d1491bfa8a4345675b8ece7ca78125bf9c8c6e97
John Ky <newhoggy@gmail.com>
2017-04-20 18:36:48 +1000

    Compare vs builtin

--- test/Check/Eq.hs
+++ test/Check/Eq.hs
@@ -7,41 +7,39 @@ module Check.Eq where
 import           Hedgehog
 import qualified Hedgehog.Gen   as Gen
 import qualified Hedgehog.Range as Range
-import           Koan.Eq
+import           Koan.Eq        as K
 import           Prelude        hiding (elem, filter)
+import qualified Prelude        as P
 
 prop_filterInt :: Property
-prop_filterInt =
-  property $ do
-    x   <- forAll $ Gen.int (Range.constantBounded)
-    xs  <- forAll $ Gen.list (Range.linear 0 100) (Gen.int (Range.constantBounded))
-    length (filterInt (== x) xs) + length (filterInt (/= x) xs) === length xs
+prop_filterInt = property $ do
+  x   <- forAll $ Gen.int (Range.constantBounded)
+  xs  <- forAll $ Gen.list (Range.linear 0 100) (Gen.int (Range.constantBounded))
+  K.filterInt (== x) xs === P.filter (== x) xs
 
 prop_filterChar :: Property
 prop_filterChar = property $ do
   x   <- forAll $ Gen.enum 'a' 'z'
   xs  <- forAll $ Gen.list (Range.linear 0 100) (Gen.enum 'a' 'z')
-  length (filterChar (== x) xs) + length (filterChar (/= x) xs) === length xs
+  K.filterChar (== x) xs === P.filter (== x) xs
 
 prop_filter :: Property
 prop_filter = property $ do
   x   <- forAll $ Gen.enum 'a' 'z'
   xs  <- forAll $ Gen.list (Range.linear 0 100) (Gen.enum 'a' 'z')
-  length (filter (== x) xs) + length (filter (/= x) xs) === length xs
+  K.filter (== x) xs === P.filter (== x) xs
 
 prop_elemInt :: Property
 prop_elemInt = property $ do
   x   <- forAll $ Gen.int (Range.constantBounded)
   xs  <- forAll $ Gen.list (Range.linear 0 100) (Gen.int (Range.constantBounded))
-  x `elemInt` (filter (/= x) xs) === False
-  x `elem` (x:xs) === True
+  (x `K.elemInt` xs) === (x `P.elem` xs)
 
 prop_elem :: Property
 prop_elem = property $ do
   x   <- forAll $ Gen.enum 'a' 'z'
   xs  <- forAll $ Gen.list (Range.linear 0 100) (Gen.enum 'a' 'z')
-  x `elem` (filter (/= x) xs) === False
-  x `elem` (x:xs) === True
+  (x `K.elem` xs) === (x `P.elem` xs)
 
 tests :: IO Bool
 tests = ($$(checkConcurrent))

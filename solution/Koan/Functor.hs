d152fe917f69b11327856952e2050adf64babc9e
John Ky <newhoggy@gmail.com>
2017-04-20 20:20:27 +1000

    Functor

--- solution/Koan/Functor.hs
+++ solution/Koan/Functor.hs
@@ -1,4 +1,34 @@
 module Koan.Functor where
 
+import           Prelude hiding (Functor, fmap, (<$), (<$>))
+
 enrolled :: Bool
 enrolled = True
+
+{- | The 'Functor' class is used for types that can be mapped over.
+Instances of 'Functor' should satisfy the following laws:
+
+> fmap id  ==  id
+> fmap (f . g)  ==  fmap f . fmap g
+
+The instances of 'Functor' for lists, 'Data.Maybe.Maybe' and 'System.IO.IO'
+satisfy these laws.
+-}
+
+class Functor f  where
+  fmap :: (a -> b) -> f a -> f b
+
+  -- | Replace all locations in the input with the same value.
+  -- The default definition is @'fmap' . 'const'@, but this may be
+  -- overridden with a more efficient version.
+  (<$) :: a -> f b -> f a
+  (<$) =  fmap . const
+
+(<$>) :: Functor f => (a -> b) -> f a -> f b
+(<$>) = fmap
+
+($>) :: Functor f => f a -> b -> f b
+($>) = flip (<$)
+
+void :: Functor f => f a -> f ()
+void x = () <$ x
--- test/Check/Functor.hs
+++ test/Check/Functor.hs
@@ -1,17 +1,46 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Check.Functor where
 
+import qualified Data.Functor   as P
 import           Hedgehog
 import qualified Hedgehog.Gen   as Gen
 import qualified Hedgehog.Range as Range
-import           Koan.Functor
+import           Koan.Functor   as K
+import           Prelude
+import qualified Prelude        as P
 
-prop_reverse :: Property
-prop_reverse =
-  property $ do
-    xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
-    reverse (reverse xs) === xs
+instance K.Functor Maybe where
+  fmap f (Just a) = Just (f a)
+  fmap _ Nothing  = Nothing
+
+prop_fmap :: Property
+prop_fmap = property $ do
+  ma <- forAll $ Gen.maybe (Gen.int Range.constantBounded)
+  ((+1) `K.fmap` ma) === ((+1) `P.fmap` ma)
+
+prop_fmap_op :: Property
+prop_fmap_op = property $ do
+  ma <- forAll $ Gen.maybe (Gen.int Range.constantBounded)
+  ((+1) K.<$> ma) === ((+1) P.<$> ma)
+
+prop_fmap_left_op :: Property
+prop_fmap_left_op = property $ do
+  a  <- forAll $ Gen.maybe Gen.alpha
+  mb <- forAll $ Gen.maybe (Gen.int Range.constantBounded)
+  (a K.<$ mb) === (a P.<$ mb)
+
+prop_fmap_right_op :: Property
+prop_fmap_right_op = property $ do
+  a  <- forAll $ Gen.maybe Gen.alpha
+  mb <- forAll $ Gen.maybe (Gen.int Range.constantBounded)
+  (mb K.$> a) === (mb P.$> a)
+
+prop_fmap_void :: Property
+prop_fmap_void = property $ do
+  mb <- forAll $ Gen.maybe (Gen.int Range.constantBounded)
+  K.void mb === P.void mb
 
 tests :: IO Bool
 tests = ($$(checkConcurrent))

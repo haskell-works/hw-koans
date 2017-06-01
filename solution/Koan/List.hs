eb33aee5adb63c52ece3f8d38c42b94f8345dd41
John Ky <newhoggy@gmail.com>
2017-06-01 10:48:38 +1000

    Add example comments

--- solution/Koan/List.hs
+++ solution/Koan/List.hs
@@ -9,37 +9,55 @@ import           Koan.Monad       as K
 enrolled :: Bool
 enrolled = False
 
+-- Example:
+--   head [1, 2, 3, 4] = 1
 head :: [a] -> a
 head (x:_) = x
 
+-- Example:
+--   tail [1, 2, 3, 4] = [2, 3, 4]
 tail :: [a] -> [a]
 tail (_:xs) = xs
 
+-- Example:
+--   last [1, 2, 3, 4] = 4
 last :: [a] -> a
 last [x] = x
 last (_:xs) = last xs
 
+-- Example:
+--   reverse [1, 2, 3, 4] = [4, 3, 2, 1]
 reverse :: [a] -> [a]
 reverse = go []
   where go rs (x:xs) = go (x:rs) xs
         go rs []     = rs
 
+-- Example:
+--   [1, 2] ++ [3, 4] = [1, 2, 3, 4]
 (++) :: [a] -> [a] -> [a]
 (++) (x:xs) ys = x:(xs ++ ys)
 (++) [] ys     = ys
 
+-- Example:
+--   concat [[1, 2], [3, 4]] = [1, 2, 3, 4]
 concat :: [[a]] -> [a]
 concat (x:xs) = x ++ concat xs
 concat []     = []
 
+-- Example:
+--   tails [1, 2, 3] = [[1, 2, 3], [2, 3], [3], []]
 tails :: [a] -> [[a]]
 tails (x:xs) = (x:xs) : tails xs
 tails []     = [[]]
 
+-- Example:
+--   mapList show [1, 2, 3, 4] = ["1", "2", "3", "4"]
 mapList :: (a -> b) -> [a] -> [b]
 mapList _ [] = []
 mapList f (x:xs) = f x : mapList f xs
 
+-- Example:
+--   filterList even [1, 2, 3, 4] = [2, 4]
 filterList :: (a -> Bool) -> [a] -> [a]
 filterList _ [] = []
 filterList p (x:xs)

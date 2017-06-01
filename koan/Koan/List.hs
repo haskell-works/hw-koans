fc3d212a99d8ca989fce9cfd18b25add8c16966a
John Ky <newhoggy@gmail.com>
2017-06-01 10:52:07 +1000

    Add example comments

--- koan/Koan/List.hs
+++ koan/Koan/List.hs
@@ -9,30 +9,48 @@ import           Koan.Monad       as K
 enrolled :: Bool
 enrolled = False
 
+-- Example:
+--   head [1, 2, 3, 4] = 1
 head :: [a] -> a
 head = error "TODO: implement head"
 
+-- Example:
+--   tail [1, 2, 3, 4] = [2, 3, 4]
 tail :: [a] -> [a]
 tail = error "TODO: implement tail"
 
+-- Example:
+--   last [1, 2, 3, 4] = 4
 last :: [a] -> a
 last = error "TODO: implement last"
 
+-- Example:
+--   reverse [1, 2, 3, 4] = [4, 3, 2, 1]
 reverse :: [a] -> [a]
 reverse = error "TODO: implement reverse"
 
+-- Example:
+--   [1, 2] ++ [3, 4] = [1, 2, 3, 4]
 (++) :: [a] -> [a] -> [a]
 (++) = error "TODO: implement (++)"
 
+-- Example:
+--   concat [[1, 2], [3, 4]] = [1, 2, 3, 4]
 concat :: [[a]] -> [a]
 concat = error "TODO: implement concat"
 
+-- Example:
+--   tails [1, 2, 3] = [[1, 2, 3], [2, 3], [3], []]
 tails :: [a] -> [[a]]
 tails = error "TODO: implement tails"
 
+-- Example:
+--   mapList show [1, 2, 3, 4] = ["1", "2", "3", "4"]
 mapList :: (a -> b) -> [a] -> [b]
 mapList = error "TODO: implement mapList"
 
+-- Example:
+--   filterList even [1, 2, 3, 4] = [2, 4]
 filterList :: (a -> Bool) -> [a] -> [a]
 filterList = error "TODO: implement filterList"
 

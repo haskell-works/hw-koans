1e97d6af5a6cb9bf2e8f197e91d8ea6afaa01864
John Ky <newhoggy@gmail.com>
2017-06-14 16:39:38 +1000

    Parser tests

--- test/Check/Parser.hs
+++ test/Check/Parser.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE FlexibleInstances    #-}
 
 module Check.Parser where
 
@@ -25,12 +26,32 @@ import qualified Hedgehog.Range as Range
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
 {-# ANN module ("HLint: ignore Redundant bracket"   :: String) #-}
 
-toKoanResult :: P.Result a -> K.ParseResult a
-toKoanResult (P.Done _ a)   = K.ParseSuccess "" a
-toKoanResult (P.Fail _ _ m) = if "Failed reading: " `isPrefixOf` m
-  then K.ParseFailure (drop 16 m)
-  else K.ParseFailure m
-toKoanResult (P.Partial _)  = K.ParseFailure "Incomplete"
+data ParsedResult a
+  = ParsedSuccess a
+  | ParsedFailure String
+  deriving (Eq, Show)
+
+class FromParsedResult f where
+  toParsedResult :: f a -> ParsedResult a
+
+instance FromParsedResult P.Result where
+  toParsedResult (P.Done _ a)   = ParsedSuccess a
+  toParsedResult (P.Fail _ _ m) = if "Failed reading: " `isPrefixOf` m
+    then ParsedFailure (drop 16 m)
+    else ParsedFailure m
+  toParsedResult (P.Partial _)  = ParsedFailure "Incomplete"
+
+instance FromParsedResult K.ParseResult where
+  toParsedResult (K.ParseSuccess _ a) = ParsedSuccess a
+  toParsedResult (K.ParseFailure m)   = ParsedFailure m
+
+instance FromParsedResult ParsedResult where
+  toParsedResult = id
+
+infixl 1 =~=
+
+(=~=) :: (Monad m, FromParsedResult f, FromParsedResult g, Eq a, Show a) => f a -> g a -> Test m ()
+(=~=) fa ga = toParsedResult fa === toParsedResult ga
 
 prop_fmap_result_op :: Property
 prop_fmap_result_op = property $ do
@@ -38,14 +59,14 @@ prop_fmap_result_op = property $ do
   ea <- forAll $ Gen.element [Right 1, Right 2, Right 3, Left "Failed Reading: failed", Left "error"]
   let kp = either K.ParseFailure    (K.ParseSuccess "") ea :: K.ParseResult Int
   let pp = either (P.Fail "xxx" []) (P.Done         "") ea :: P.Result Int
-  ((+1) <$> kp) === toKoanResult ((+1) <$> pp)
+  (+1) <$> kp =~= (+1) <$> pp
 
 prop_fail :: Property
 prop_fail = property $ do
   ea <- forAll $ Gen.element ["Failed Reading: failed", "error"]
   let ka = K.fail ea :: K.Parser Int
   let pa = P.fail ea :: P.Parser Int
-  K.runParser ka "" === toKoanResult (P.parse pa "")
+  K.runParser ka "" =~= P.parse pa ""
 
 prop_fmap_op :: Property
 prop_fmap_op = property $ do
@@ -54,7 +75,7 @@ prop_fmap_op = property $ do
   ea <- forAll $ Gen.element [Right 1, Right 2, Right 3, Left "Failed Reading: failed", Left "error"]
   let kp = either fail'  pure' ea :: K.Parser Int
   let pp = either P.fail pure  ea :: P.Parser Int
-  K.runParser ((+1) <$> kp) "" === toKoanResult (P.parse ((+1) <$> pp) "")
+  K.runParser ((+1) <$> kp) "" =~= P.parse ((+1) <$> pp) ""
 
 prop_pure :: Property
 prop_pure = property $ do
@@ -63,7 +84,7 @@ prop_pure = property $ do
   ea <- forAll $ Gen.element [Right 1, Right 2, Right 3, Left "Failed Reading: failed", Left "error"]
   let ka = either fail'  pure ea :: K.Parser Int
   let pa = either P.fail pure ea :: P.Parser Int
-  K.runParser ka "" === toKoanResult (P.parse pa "")
+  K.runParser ka "" =~= P.parse pa ""
 
 prop_apply_op :: Property
 prop_apply_op = property $ do
@@ -74,14 +95,14 @@ prop_apply_op = property $ do
   let ka = either fail'  pure ea :: K.Parser Int
   let pa = either P.fail pure ea :: P.Parser Int
   let pb = either P.fail pure eb :: P.Parser Int
-  K.runParser ((,) <$> ka <*> kb) "" === toKoanResult (P.parse ((,) <$> pa <*> pb) "")
+  K.runParser ((,) <$> ka <*> kb) "" =~= P.parse ((,) <$> pa <*> pb) ""
 
 prop_empty :: Property
 prop_empty = property $ do
   ea <- forAll $ Gen.element ["abc", "def" :: String]
   let ka = empty :: K.Parser Int
   let pa = empty :: P.Parser Int
-  K.runParser ka "" === toKoanResult (P.parse pa "")
+  K.runParser ka "" =~= P.parse pa ""
 
 prop_orElse_op :: Property
 prop_orElse_op = property $ do
@@ -92,7 +113,7 @@ prop_orElse_op = property $ do
   let kb = either fail'  pure eb :: K.Parser Int
   let pa = either P.fail pure ea :: P.Parser Int
   let pb = either P.fail pure eb :: P.Parser Int
-  K.runParser (ka <|> kb) "" === toKoanResult (P.parse (pa <|> pb) "")
+  K.runParser (ka <|> kb) "" =~= P.parse (pa <|> pb) ""
 
 prop_satisfy :: Property
 prop_satisfy = property $ do
@@ -100,7 +121,7 @@ prop_satisfy = property $ do
   eb <- forAll $ Gen.element "abcdef"
   let ka = K.satisfy (== ea) :: K.Parser Char
   let pa = P.satisfy (== ea) :: P.Parser Char
-  K.runParser ka (fromString [eb]) === toKoanResult (P.parse pa (fromString [eb]))
+  K.runParser ka (fromString [eb]) =~= P.parse pa (fromString [eb])
 
 prop_char :: Property
 prop_char = property $ do
@@ -108,7 +129,7 @@ prop_char = property $ do
   eb <- forAll $ Gen.element "abcdef"
   let ka = K.char ea :: K.Parser Char
   let pa = P.char ea :: P.Parser Char
-  K.runParser ka (fromString [eb]) === toKoanResult (P.parse pa (fromString [eb]))
+  K.runParser ka (fromString [eb]) =~= P.parse pa (fromString [eb])
 
 prop_notChar :: Property
 prop_notChar = property $ do
@@ -116,67 +137,63 @@ prop_notChar = property $ do
   eb <- forAll $ Gen.element "abcdef"
   let ka = K.notChar ea :: K.Parser Char
   let pa = P.notChar ea :: P.Parser Char
-  K.runParser ka (fromString [eb]) === toKoanResult (P.parse pa (fromString [eb]))
+  K.runParser ka (fromString [eb]) =~= P.parse pa (fromString [eb])
 
 prop_anyChar :: Property
 prop_anyChar = property $ do
   ea <- forAll $ Gen.string (Range.linear 0 10) (Gen.element "abcdef")
   let ka = K.anyChar :: K.Parser Char
   let pa = P.anyChar :: P.Parser Char
-  K.runParser ka (fromString ea) === toKoanResult (P.parse pa (fromString ea))
+  K.runParser ka (fromString ea) =~= P.parse pa (fromString ea)
 
 prop_peekChar :: Property
 prop_peekChar = property $ do
   ea <- forAll $ Gen.string (Range.linear 0 10) (Gen.element "abcdef")
   let ka = K.peekChar :: K.Parser (Maybe Char)
   let pa = P.peekChar :: P.Parser (Maybe Char)
-  K.runParser ka (fromString ea) === toKoanResult (P.parse pa (fromString ea))
+  K.runParser ka (fromString ea) =~= P.parse pa (fromString ea)
 
--- prop_skip :: Property
--- prop_skip = property $ do
---   ea <- forAll $ Gen.string (Range.linear 0 10) (Gen.element "abcdef")
---   eb <- forAll $ Gen.string (Range.linear 0 3 ) (Gen.element "abcdef")
---   let ka = K.skip (`elem`                     eb ) :: K.Parser ()
---   let pa = P.skip (`elem` BS.unpack (BSC.pack eb)) :: P.Parser ()
---   K.runParser ka (fromString ea) === toKoanResult (P.parse pa (fromString ea))
+prop_skip :: Property
+prop_skip = property $ do
+  ea <- forAll $ Gen.string (Range.linear 0 10) (Gen.element "abcdef")
+  eb <- forAll $ Gen.string (Range.linear 1 3 ) (Gen.element "abcdef")
+  let ka = K.skip (`elem`                     eb ) :: K.Parser ()
+  let pa = P.skip (`elem` BS.unpack (BSC.pack eb)) :: P.Parser ()
+  K.runParser ka (fromString ea) =~= P.parse pa (fromString ea)
+
+prop_digit :: Property
+prop_digit = property $ do
+  ea <- forAll $ Gen.string (Range.linear 0 2) (Gen.element "ABCDEFabcdef0123 ")
+  K.runParser K.digit (fromString ea) =~= P.parse P.digit (fromString ea)
+
+prop_letter :: Property
+prop_letter = property $ do
+  ea <- forAll $ Gen.string (Range.linear 0 2) (Gen.element "ABCDEFabcdef0123 ")
+  K.runParser K.letter (fromString ea) =~= P.parse P.letter_ascii (fromString ea)
+
+prop_space :: Property
+prop_space = property $ do
+  ea <- forAll $ Gen.string (Range.linear 0 2) (Gen.element "ABCDEFabcdef0123 ")
+  K.runParser K.space (fromString ea) =~= P.parse P.space (fromString ea)
+
+prop_string :: Property
+prop_string = property $ do
+  ea <- forAll $ Gen.string (Range.linear 0 10) (Gen.element "10")
+  eb <- forAll $ Gen.string (Range.linear 0  3) (Gen.element "10")
+  K.runParser (K.string eb) (fromString ea) =~= BSC.unpack <$> P.parse (P.string (fromString eb)) (fromString ea)
+
+prop_doubleQuoted :: Property
+prop_doubleQuoted = property $ do
+  ea <- forAll $ Gen.string (Range.linear 0 10) (Gen.element "abc\"\\")
+  eb <- forAll $ Gen.choice [pure (show ea)]
+  K.runParser K.doubleQuoted (fromString eb) =~= ParsedSuccess ea
 
 {-
-skip :: (Char -> Bool) -> Parser ()
-skip p = satisfy p *> pure ()
-
-peekChar :: Parser (Maybe Char)
-peekChar = Parser $ \s -> case s of
-  (a:as) -> ParseSuccess s (Just a)
-  _      -> ParseSuccess s Nothing
-
-peekChar' :: Parser Char
-peekChar' = Parser $ \s -> case s of
-  (a:as) -> ParseSuccess s a
-  _      -> ParseFailure "unexpected end of stream"
-
-digit :: Parser Char
-digit = satisfy (\c -> '0' <= c && c <= '9')
-
-letter :: Parser Char
-letter = satisfy isAlpha
-
-space :: Parser Char
-space = satisfy isSpace
-
-string :: String -> Parser String
-string text = Parser $ \s -> if text `isPrefixOf` s
-  then ParseSuccess (drop (length text) s) text
-  else ParseFailure ("expected string " <> show text)
-
 doubleQuoted :: Parser String
 doubleQuoted = char '"' *> many escapedChar <* char '"'
   where escapedChar = satisfy (`notElem` chars) <|> (char '\\' *> satisfy (`elem` chars))
         chars       = "\\\"\n\t\r"
-
-
 -}
-
-
 
 tests :: IO Bool
 tests = checkSequential $ reversed $$(discover)

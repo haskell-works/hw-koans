32cb52bf7c023d0a53db01f2bf3a8f22eb310f7c
John Ky <newhoggy@gmail.com>
2017-07-26 17:00:27 +1000

    New escapedChar test

--- koan/Koan/Parser/Json.hs
+++ koan/Koan/Parser/Json.hs
@@ -35,6 +35,9 @@ symbol = L.symbol sc
 escapees :: String
 escapees = "\\\"\n\r\t"
 
+escapeeChars :: String
+escapeeChars = "\\\"nrt"
+
 -- Hint: Use between function
 brackets :: Parser a -> Parser a
 brackets = error "TODO Implement brackets"

cd76e6366e054b2d0fdfb87bb733b5a32026fa58
John Ky <newhoggy@gmail.com>
2017-07-27 10:39:59 +1000

    Test stubs

--- koan/Koan/Parser/Json/Test.hs
+++ koan/Koan/Parser/Json/Test.hs
@@ -70,103 +70,83 @@ toString  K.JsonNull       = "null"
 
 prop_comma_matched :: Property
 prop_comma_matched = property $ do
-  parse K.comma "" "," === Right ()
+  error "TODO: Implement prop_comma_matched"
 
 prop_comma_unmatched :: Property
 prop_comma_unmatched = property $ do
-  nonComma <- forAll $ (/= ',') `Gen.filter` Gen.ascii
-  parse K.comma "" [nonComma] /== Right ()
+  error "TODO: Implement prop_comma_unmatched"
 
 prop_plainChar_matched :: Property
 prop_plainChar_matched = property $ do
-  nonEscapee <- forAll $ (`notElem` K.escapees) `Gen.filter` Gen.ascii
-  parse K.plainChar "" [nonEscapee] === Right nonEscapee
+  error "TODO: Implement prop_plainChar_matched"
 
 prop_plainChar_unmatched :: Property
 prop_plainChar_unmatched = property $ do
-  escapee <- forAll $ Gen.element K.escapees
-  parse K.plainChar "" [escapee] ?== isLeft
+  error "TODO: Implement prop_plainChar_unmatched"
 
 prop_escapedChar_matched :: Property
 prop_escapedChar_matched = property $ do
-  nonEscapee  <- forAll $ (`elem` K.escapeeChars) `Gen.filter` Gen.ascii
-  text        <- forAll $ pure ['\\', nonEscapee]
-  parse K.escapedChar "" text === Right (read ['\'', '\\', nonEscapee, '\''])
+  error "TODO: Implement prop_escapedChar_matched"
 
 prop_escapedChar_unmatched_0 :: Property
 prop_escapedChar_unmatched_0 = property $ do
-  c <- forAll Gen.ascii
-  parse K.escapedChar "" [c] ?== isLeft
+  error "TODO: Implement prop_escapedChar_unmatched_0"
 
 prop_escapedChar_unmatched_1 :: Property
 prop_escapedChar_unmatched_1 = property $ do
-  nonEscapee  <- forAll $ (`notElem` K.escapeeChars) `Gen.filter` Gen.ascii
-  text        <- forAll $ pure ['\\', nonEscapee]
-  parse K.escapedChar "" text ?== isLeft
+  error "TODO: Implement prop_escapedChar_unmatched_1"
 
 prop_litBool_matched :: Property
 prop_litBool_matched = property $ do
-  text <- forAll $ Gen.element ["true", "false"]
-  parse K.litBool "" text === Right (text == "true")
+  error "TODO: Implement prop_litBool_matched"
 
 prop_litBool_unmatched :: Property
 prop_litBool_unmatched = property $ do
-  text <- forAll $ (`notElem` ["true", "false"]) `Gen.filter` Gen.string (Range.linear 4 5) Gen.alpha
-  parse K.litBool "" text ?== isLeft
+  error "TODO: Implement prop_litBool_unmatched"
 
 prop_brackets_matched :: Property
 prop_brackets_matched = property $ do
-  pad1 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  pad2 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  pad3 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  text <- forAll $ pure $ "[" ++ pad1 ++ "true" ++ pad2 ++ "]" ++ pad3
-  parse (K.brackets K.litBool) "" text === Right True
+  error "TODO: Implement prop_brackets_matched"
 
 prop_brackets_unmatched :: Property
 prop_brackets_unmatched = property $ do
-  pad1 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  pad2 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  text <- forAll $ pure $ "[" ++ pad1 ++ "true" ++ pad2
-  parse (K.brackets K.litBool) "" text ?== isLeft
+  error "TODO: Implement prop_brackets_unmatched"
 
 prop_braces_matched :: Property
 prop_braces_matched = property $ do
-  pad1 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  pad2 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  pad3 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  text <- forAll $ pure $ "{" ++ pad1 ++ "true" ++ pad2 ++ "}" ++ pad3
-  parse (K.braces K.litBool) "" text === Right True
+  error "TODO: Implement prop_braces_matched"
 
 prop_braces_unmatched :: Property
 prop_braces_unmatched = property $ do
-  pad1 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  pad2 <- forAll $ Gen.string (Range.linear 0 2) (pure ' ')
-  text <- forAll $ pure $ "{" ++ pad1 ++ "true" ++ pad2
-  parse (K.braces K.litBool) "" text ?== isLeft
+  error "TODO: Implement prop_braces_unmatched"
 
 prop_litString :: Property
-prop_litString = property $ error "TODO Implement prop_litString"
+prop_litString = property $ do
+  error "TODO Implement prop_litString"
 
 prop_array :: Property
-prop_array = property $ error "TODO Implement prop_array"
+prop_array = property $ do
+  error "TODO Implement prop_array"
 
 prop_field :: Property
-prop_field = property $ error "TODO Implement prop_field"
+prop_field = property $ do
+  error "TODO Implement prop_field"
 
 prop_object :: Property
-prop_object = property $ error "TODO Implement prop_object"
+prop_object = property $ do
+  error "TODO Implement prop_object"
 
 prop_nullKeyword :: Property
-prop_nullKeyword = property $ error "TODO Implement prop_nullKeyword"
+prop_nullKeyword = property $ do
+  error "TODO Implement prop_nullKeyword"
 
 prop_number :: Property
-prop_number = property $ error "TODO Implement prop_number"
+prop_number = property $ do
+  error "TODO Implement prop_number"
 
 prop_json :: Property
 prop_json = property $ do
-  j <- forAll   genJson
-  s <- forAll $ pure (toString j)
-  parse K.json "" s === Right j
+  error "TODO Implement prop_json"
 
 tests :: IO Bool
 tests = checkSequential $ reversed $$discover
